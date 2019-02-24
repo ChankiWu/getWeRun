e340d5449ef50c1e7f5febb4fe8b508b7b6f6b51
QianchengWu <254152970@qq.com>
2019-02-24 02:17:30 -0800

    Employ cloud function to replace old nodejs server, 30 days done.

--- miniprogram/pages/addFunction/addFunction.js
+++ miniprogram/pages/addFunction/addFunction.js
@@ -4,16 +4,6 @@ const appid = "wxe89011b6aaa0836e";
 const secretkey = "954a3b6622b1579457a633a51cc15202";
 
 var app = getApp();
-const domain = "http://52.40.139.185:3000/";
-
-const code = `// 云函数入口函数
-exports.main = (event, context) => {
-  console.log(event)
-  console.log(context)
-  return {
-    sum: event.a + event.b
-  }
-}`
 
 Page({
 
@@ -24,76 +14,83 @@ Page({
   },
 
   onLoad: function (options) {
-
+      wx.cloud.init();
       var that = this;
+      
       wx.login({
         success: function (resLogin) {
           if (resLogin.code) {
-            wx.request({
-              url: domain + 'login',
-              data: {
+            
+            //调用云函数获取session_key
+            wx.cloud.callFunction({
+              name:'login',
+              data:{
+                appid:appid,
+                secretkey: secretkey,
                 code: resLogin.code
               },
-              fail: function (message) {
-                console.log("fail to get response from nodejs server" + message);
-              },
-              success: function (resSession) {
-                console.log("Session", resSession.data);
+            }).then(res => {
+              console.log("Session_key ", res.result.session_key);
+              var session_key = res.result.session_key;
+              //getUserinfo
+              wx.getSetting({
+                success: function (res) {
+                  //先确定用户是谁
+                  wx.getUserInfo({
+                    success: user_res => {
+                      console.log("Userinfo",user_res.userInfo.nickName);
 
-                //getUserinfo
-                wx.getSetting({
-                  success: function (res) {
+                      wx.getWeRunData({
+                        success(resRun) {
 
-                    wx.getWeRunData({
-                      success(resRun) {
-
-                        wx.request({
-                          url: domain + 'result',
-                          data: {
-                            encryptedData: resRun.encryptedData,
-                            iv: resRun.iv,
-                            session_key: resSession.data
-                          },
-                          success: function (response) {
-
+                          //调用云函数获取30天运动数据
+                          wx.cloud.callFunction({
+                            name: 'getRundata',
+                            data: {
+                              encryptedData: resRun.encryptedData,
+                              iv: resRun.iv,
+                              session_key: session_key,
+                              user_info: user_res.userInfo.nickName
+                            },
+                          }).then(res => {
                             var run_data = new Array();
                             //简单处理一下
-                            for (var i in response.data.data.stepInfoList) {
-                              run_data.push("时间："+response.data.data.stepInfoList[i].date+ " 步数：" + response.data.data.stepInfoList[i].step)
+                            for (var i in res.result.data.stepInfoList) {
+                              run_data.push("时间：" + res.result.data.stepInfoList[i].date + " 步数：" + res.result.data.stepInfoList[i].step)
                             }
-
                             console.log("Done.", run_data);
                             that.setData({
                               run: run_data
                             })
 
-                          }
-                        })
-                        
-                      },
+                          })
+                        },
+                        fail: function (res) {
+                          wx.showModal({
+                            title: '提示',
+                            content: '开发者未开通微信运动，请关注“微信运动”公众号后重试',
+                            showCancel: false,
+                            confirmText: '知道了'
+                          })
+                        }
 
-                      fail: function (res) {
-                        wx.showModal({
-                          title: '提示',
-                          content: '开发者未开通微信运动，请关注“微信运动”公众号后重试',
-                          showCancel: false,
-                          confirmText: '知道了'
-                        })
-                      }
+                      })  //getrundata
 
-                    })  //getrundata
+                    },
+                    fail: err =>{
+                      console.log("Error to get user info");
+                    }
+                  })//getuserinfo
+             
 
-                  }
+                }
 
-                })
-
-              },
-
-            })  //request
+              })//getsetting
+            })
           }
         }
-      })  //login
-    
+      })//login
+           
   },
 
   onShow: function () {
